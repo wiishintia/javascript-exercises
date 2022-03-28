5b15421afdcfd1aa6d00c3f40c940af3d7736e23
Dewi Shintia <wiishintia@gmail.com>
2022-03-28 22:24:35 +1000

    find the oldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const yearNow = new Date().getFullYear();
+    return people.reduce((acc,value) => {
+        if (value.yearOfDeath == undefined || acc.yearOfDeath == undefined){
+            return (yearNow- acc.yearOfBirth > yearNow- value.yearOfBirth)? acc : value
+        } else {
+            return (acc.yearOfDeath - acc.yearOfBirth >value.yearOfDeath - value.yearOfBirth)? acc : value
+        }
+        
+    })
 };
 
 // Do not edit below this line
