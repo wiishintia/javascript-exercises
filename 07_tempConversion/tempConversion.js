7f278aec74e74acf7934fb8dc9992cc63b754489
Dewi Shintia <wiishintia@gmail.com>
2022-03-28 21:01:44 +1000

    git tempconversion is done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+    return Math.round((tempF-32)*5/9*10)/10
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  return Math.round((tempC*9/5+32)*10)/10
 };
 
 // Do not edit below this line
