e7207a5e8e0db152e85a906b2fa1deb58b1966b9
Dewi Shintia <wiishintia@gmail.com>
2022-03-28 22:25:22 +1000

    caesar is done

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,24 @@
-const caesar = function() {
-
+const caesar = function(text,num) {
+    let newText = "";
+    for(let i=0;i<text.length;i++){
+        if (text.charCodeAt(i)>= 65 && text.charCodeAt(i) <= 90){
+            if (num <0){
+                newText += String.fromCharCode((text.charCodeAt(i) - 65 +num + 26)%26 + 65);
+            } else {
+                newText += String.fromCharCode((text.charCodeAt(i) - 65 +num)%26 + 65);
+            }
+        } else if (text.charCodeAt(i)>=97 && text.charCodeAt(i) <=122){
+            if (num <0){
+                newText += String.fromCharCode((text.charCodeAt(i) - 97 +num + 26)%26 + 97);
+            } else {
+                newText += String.fromCharCode((text.charCodeAt(i) - 97 +num)%26 + 97);
+            }
+        } else{
+            newText += String.fromCharCode(text.charCodeAt(i));
+        }
+        
+    }
+    return newText;
 };
 
 // Do not edit below this line
