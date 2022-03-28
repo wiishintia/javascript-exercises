5c378e02cb9406eb50cec47eba2dc1c485d51050
Dewi Shintia <wiishintia@gmail.com>
2022-03-28 22:09:00 +1000

    palindrome done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let cleantext = text.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()/\s]/g, '').split("");
+    let l = cleantext.length;
+    for (let i=0;i<l/2;i++){
+        if(cleantext[i] !== cleantext[l-1-i]){
+            return false;
+        }
+    }
+    return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
