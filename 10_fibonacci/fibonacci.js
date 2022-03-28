446927c2329ef2899d6b3c37b82b646ba8ecb8cc
Dewi Shintia <wiishintia@gmail.com>
2022-03-28 22:09:59 +1000

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    // let arr=[0,1];
+    // if (x<0){
+    //     return "OOPS";
+    // } else {
+    //     for (let i=2;i<=x;i++){
+    //         arr[i]=arr[i-2]+ arr[i-1];
+    //     }
+    // }
+    // return arr[x];
+    if (x==0){
+        return 0;
+    }
+    if (x==1 || x==2){
+        return 1;
+    } else {
+        return fibonacci(x-1)+fibonacci(x-2);
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
