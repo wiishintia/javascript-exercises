b4c4375737ea644670eb2b4424e8449eae6ef00d
Dewi Shintia <wiishintia@gmail.com>
2022-03-28 20:57:38 +1000

    sum All is done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,10 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    let n = Math.max(x,y)-Math.min(x,y)+1;
+    if ((x>=0 && y>=0) && (Number.isInteger(x) && Number.isInteger(y))){
+        return n*(Math.min(x,y)+ Math.max(x,y))/2;
+    } else {
+        return "ERROR";
+    }
 
 };
 
