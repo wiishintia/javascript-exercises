aebf997627b3bf5f6cace3f419e52ffd5c05dd41
Dewi Shintia <wiishintia@gmail.com>
2022-03-28 20:56:17 +1000

    remove from Array done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myarray,...xs) {
+    for (let i=0; i<myarray.length;i++){
+        for (let x of xs){
+            if (myarray[i] === x){
+                myarray.splice(i,1);
+            }
+        }
+    }
+    return myarray;    
+    
 };
 
 // Do not edit below this line
