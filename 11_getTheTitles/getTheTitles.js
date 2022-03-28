4e7059327c520997654774cffd9e61baebb202b3
Dewi Shintia <wiishintia@gmail.com>
2022-03-28 22:21:51 +1000

    get the titles done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    // let listTitles =[];
+    // for (let i =0;i<books.length;i++){
+    //     listTitles.push(books[i].title);
+    // }
+    // return listTitles
+    return books.map(x=> x.title)
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
